7e86f4cb702ff73a125009df1baaa149857124f0
rmichie <michie.ryan@deq.state.or.us>
2016-03-18 11:28:43 -0700

    script to convert inputs from long to wide format

---
+++ long2wide.R
@@ -0,0 +1,65 @@
+# converts heat source landcover input from long to wide format
+# outputs as csv
+
+library(reshape2)
+
+options(echo=TRUE)
+
+outpath <- commandArgs(trailingOnly = TRUE)
+
+outpath <- "C:/WorkSpace/Quantifying_Conservation_2014/SouthernWillamette/Heat_Source/01_Current_ObsEffectiveShade/"
+
+df.long <- read.table(paste0(outpath,"Obs_Effective_Shade_v1/inputs/lcdata.txt"),
+                      sep=",",dec=".",
+                      skip=0, header=TRUE, 
+                      stringsAsFactors = FALSE, 
+                      na.strings = "NA")
+
+
+# This is for ordering the wide col names below
+# needs to be done before rebuilding the LC/ELE names
+df.long$ORDER <-0
+df.long$ORDER <- ifelse(df.long$VARIABLE=="LONGITUDE",1,df.long$ORDER)
+df.long$ORDER <- ifelse(df.long$VARIABLE=="LATITUDE",2,df.long$ORDER)
+df.long$ORDER <- ifelse(df.long$VARIABLE=="TOPO_W",3,df.long$ORDER)
+df.long$ORDER <- ifelse(df.long$VARIABLE=="TOPO_S",4,df.long$ORDER)
+df.long$ORDER <- ifelse(df.long$VARIABLE=="TOPO_E",5,df.long$ORDER)
+df.long$ORDER <- ifelse(df.long$VARIABLE=="LC",6,df.long$ORDER)
+df.long$ORDER <- ifelse(df.long$VARIABLE=="ELE",7,df.long$ORDER)
+
+# Reconstruct the LC and ELE col names
+df.long$VARIABLE <- ifelse(df.long$VARIABLE == "LC", 
+                            paste0(df.long$VARIABLE,
+                                   "_T",df.long$TRANSECT,
+                                   "_S",df.long$SAMPLE),
+                            df.long$VARIABLE)
+
+df.long$VARIABLE <- ifelse(df.long$VARIABLE == "ELE", 
+                            paste0(df.long$VARIABLE,
+                                   "_T",df.long$TRANSECT,
+                                   "_S",df.long$SAMPLE),
+                            df.long$VARIABLE)
+
+# cast to wide
+df.wide <- dcast(df.long, STREAM_ID + NODE_ID + STREAM_KM ~ VARIABLE, value.var="VALUE",drop=TRUE)
+
+# make a vector of the wide col names in order
+df.long <- df.long[with(df.long, order(ORDER,-STREAM_KM,TRANSECT,SAMPLE)), ]
+col.names <- c("STREAM_ID","NODE_ID","STREAM_KM", as.character(unique(df.long$VARIABLE)))
+
+# sort on col order
+df.wide <- df.wide[,col.names]
+# row order
+df.wide <- df.wide[with(df.wide,order(-STREAM_KM)),]
+
+
+
+
+
+
+write.table(df.wide, file=paste0(outpath,"Obs_Effective_Shade_v1/inputs/lcdata.csv"),
+            sep = ",",
+            dec = ".", 
+            quote = FALSE, 
+            row.names = FALSE, 
+            col.names = TRUE)
