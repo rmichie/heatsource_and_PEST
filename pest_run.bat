bf29212df193cf0dbbe5fe7ffaa666cba14d7ea9
rmichie <michie.ryan@deq.state.or.us>
2016-03-23 16:12:13 -0700

    added additional documentation

--- pest_run.bat
+++ pest_run.bat
@@ -7,16 +7,27 @@ set TOP_DIR=%~dp0
 :: replace backslash
 set "TOP_DIR=%TOP_DIR:\=/%"
 
-:: path from top directory to model directory
-set MOD_DIR="Obs_Effective_Shade_v1/inputs/"
+:: path from top directory to heat source model input directory
+set MOD_DIR="Obs_Effective_Shade_v1/hs_inputs/"
 
-:: path from top directory to dummy directory
+:: path from top directory to directory where pest writes the dummy input
 set DUM_DIR="/"
 
-:: apply PEST dummy inputs to heat source inputs
-:: last two args are pest dummy file name and heat source input name
-%R_exe% R\lccodes_dummy.R %TOP_DIR% %MOD_DIR% %DUM_DIR% lccodes_dummy.csv start_lccodes_emergent.csv lccodes_emergent.csv
-%R_exe% R\lcdata_dummy.R %TOP_DIR% %MOD_DIR% %DUM_DIR% lcdata_dummy.csv start_lcdata_emergent.csv lcdata_emergent.csv
+:: Apply the information PEST wrote to th edummy inputs into the heat source inputs
+:: The R scripts do this work but they are executed from here
+::
+:: Arguments passed to R script:
+:: 1. R executable
+:: 2. path from top directory to the R script being executed
+:: 3. TOP_DIR (see above)
+:: 4. MOD_DIR (see above)
+:: 5. DUM_DIR (see above)
+:: 6. the name of the input dummy file PEST writes (based on template file)- located in DUM_DIR
+:: 7. the name of the starting heat source input file
+:: 8. the name of the input file heat source will read
+:: last three args are pest dummy input file name and heat source input file name
+%R_exe% R\lccodes_dummy.R %TOP_DIR% %MOD_DIR% %DUM_DIR% ptf_lccodes_dummy.csv start_lccodes_emergent.csv lccodes_emergent.csv
+%R_exe% R\lcdata_dummy.R %TOP_DIR% %MOD_DIR% %DUM_DIR% ptf_lcdata_dummy.csv start_lcdata_emergent.csv lcdata_emergent.csv
 
 :: run heat heatsource
 python Obs_Effective_Shade_v1\HS9_Run_Solar_Only.py
